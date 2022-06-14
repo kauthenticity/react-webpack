7ca94090101ae80915cd94d8593e684686fde515
kauthenticity <kauthenticity@gmail.com>
2022-06-14 16:46:25 +0900

    [feat]: added typescript loader on webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js", // 번들링을 시작할 js 파일의 위치
+  entry: "./src/index.tsx", // 번들링을 시작할 js 파일의 위치
   output: {
     filename: "bundle.[hash].js", // 번들링 결과 생성된 js 파일이 저장될 경로
   },
@@ -29,6 +29,11 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.tsx?$/, // .tsx 파일에 대해
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
     ],
   },
   plugins: [
@@ -40,5 +45,8 @@ module.exports = {
     host: "localhost",
     port: port,
     open: true,
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
   },
 }
